9f5af9d919a3f82009be2a238ae5bbfb5b6bd992
JavierMonton <jmlarraz@gmail.com>
2025-07-16 09:15:33 +0200

    blogSidebarCount to 20.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -55,6 +55,7 @@ const config = {
          */
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 20,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
