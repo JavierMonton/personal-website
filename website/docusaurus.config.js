6aecda6993b74b481b9d971ca2a2af8bb5ee9e77
JavierMonton <jmlarraz@gmail.com>
2026-05-21 19:08:12 +0200

    social card

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -71,8 +71,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.png',
       navbar: {
         title: 'Home',
         logo: {
